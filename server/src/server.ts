f84ff9ff42a8f0882b91237505751f27037eceb9
vityudi <vitoryudi.artur@gmail.com>
2025-07-09 12:02:36 -0300

    aula 2 30:00 feat: add create question get questions

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomsQuestions } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
@@ -26,6 +27,7 @@ app.get('/health', () => {
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomsQuestions)
+app.register(createQuestionRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   // biome-ignore lint/suspicious/noConsole: only for dev
