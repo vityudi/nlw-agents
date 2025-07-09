fc38a91d59b0f7806f0556241855ab454a41b6a6
vityudi <vitoryudi.artur@gmail.com>
2025-07-08 23:51:58 -0300

    aula 2 - 24:51

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,8 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomsQuestions } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify()
@@ -22,7 +24,10 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomsQuestions)
 
 app.listen({ port: env.PORT }).then(() => {
+  // biome-ignore lint/suspicious/noConsole: only for dev
   console.log(`Server is running!! http://localhost:${env.PORT}`)
 })
